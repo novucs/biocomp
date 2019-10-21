07841725af70d562753f219a065acd6d999e8735
novucs <contact@novucs.net>
2019-10-21 01:52:50 +0100

    Refactor tree settings

--- biocomp/treeprogrammer.py
+++ biocomp/treeprogrammer.py
@@ -1,6 +1,12 @@
 import copy
+import itertools
 import random
-from typing import List, Generator, Union, Tuple
+from typing import (
+    Generator,
+    List,
+    Tuple,
+    Union,
+)
 
 from biocomp import datasets
 
@@ -14,67 +20,6 @@ from biocomp import datasets
 #  2   +
 #     / \
 #    f1 f2
-#
-# old representation:
-# gene = (threshold, ('+', (2, ('+', ('f1', 'f2')))))
-#
-# new representation:
-# gene = {
-#   'type': 'root',
-#   'threshold': 1,
-#   'children': [
-#       {
-#           'type': 'addition',
-#           'children': [
-#               {
-#                   'type': 'constant',
-#                   'value': 2
-#               },
-#               {
-#                   'type': 'addition',
-#                   'children': [
-#                       {
-#                           'type': 'feature',
-#                           'value': 1
-#                       },
-#                       {
-#                           'type': 'feature',
-#                           'value': 2
-#                       }
-#                   ]
-#               }
-#           ]
-#       }
-#   ]
-# }
-
-# def inner_evaluate_child(child, features) -> int:
-#     if isinstance(child, int):
-#         return child
-#     if isinstance(child, str) and child.startswith('f'):
-#         return features[int(child[1:])]
-#     if isinstance(child, tuple):
-#         return inner_evaluate(child, features)
-#
-#
-# def inner_evaluate(gene, features):
-#     operator, children = gene
-#     children = [inner_evaluate_child(child, features) for child in children]
-#
-#     if operator == '+':
-#         return sum(children)
-#
-#     if operator == '%':
-#         return children[0] % children[1]
-#
-#     raise ValueError('Unknown operation:', operator)
-#
-#
-# def evaluate(gene, features):
-#     threshold, children = gene
-#     result = inner_evaluate(children, features)
-#     return 1 if result > threshold else 0
-
 
 class Node:
     def evaluate(self, features):
@@ -168,16 +113,15 @@ class RootNode(SubtreeNode):
                    for f, l in zip(features, labels)) / len(features)
 
 
-def create_gene_inner(ls, gs) -> Node:
-    ls = copy.deepcopy(ls)
-    ls['depth'] += 1
+def create_gene_inner(settings, depth=0) -> Node:
+    depth += 1
 
-    def create_node(settings):
-        wheel = [gs[setting] for setting in settings.keys()]
+    def create_node(node_create_settings):
+        wheel = [settings[setting] for setting in node_create_settings.keys()]
         index = random.uniform(0, sum(wheel))
         cumulative = 0
-        s = next(iter(settings.values()))
-        for s, w in zip(settings.values(), wheel):
+        s = next(iter(node_create_settings.values()))
+        for s, w in zip(node_create_settings.values(), wheel):
             cumulative += w
             if w >= index:
                 break
@@ -186,18 +130,11 @@ def create_gene_inner(ls, gs) -> Node:
     # Leaf creation
 
     def create_leaf_feature():
-        if gs['only_use_features_once']:
-            if len(gs['unused_features']) <= 0:
-                return create_leaf_constant()
-
-            feature = random.choice(gs['unused_features'])
-            gs['unused_features'].remove(feature)
-        else:
-            feature = random.randint(0, gs['feature_count'] - 1)
-        return FeatureLeafNode(feature)
+        return FeatureLeafNode(random.randint(0, settings['feature_count'] - 1))
 
     def create_leaf_constant():
-        return ConstantLeafNode(random.uniform(gs['constant_min'], gs['constant_max']))
+        return ConstantLeafNode(random.uniform(
+            settings['constant_min'], settings['constant_max']))
 
     def create_leaf():
         return create_node({
@@ -208,7 +145,7 @@ def create_gene_inner(ls, gs) -> Node:
     # Subtree creation
 
     def create_children(count=2):
-        return [create_gene_inner(ls, gs) for _ in range(count)]
+        return [create_gene_inner(settings, depth) for _ in range(count)]
 
     def create_subtree_addition():
         return AdditionSubtreeNode(create_children())
@@ -230,47 +167,19 @@ def create_gene_inner(ls, gs) -> Node:
             'odds_subtree_*': create_subtree_multiply,
         })
 
-    return create_leaf() if ls['depth'] >= gs['max_depth'] else create_node({
+    return create_leaf() if depth >= settings['max_depth'] else create_node({
         'odds_leaf': create_leaf,
         'odds_subtree': create_subtree,
     })
 
 
-def create_gene_threshold():
-    return random.uniform(-1.0, 1.0)
+def create_gene_threshold(settings):
+    return random.uniform(settings['threshold_min'], settings['threshold_max'])
 
 
-def create_gene_global_settings(feature_count):
-    return {
-        'feature_count': feature_count,
-        'max_depth': 8,
-        'only_use_features_once': True,
-        'unused_features': list(range(feature_count)),
-        'constant_max': 1.0,
-        'constant_min': -1.0,
-        'odds_subtree': 1.0,
-        'odds_leaf': 1.0,
-        'odds_leaf_feature': 8.0,
-        'odds_leaf_constant': 1.0,
-        'odds_leaf_features_sum': 1.0,
-        'odds_subtree_+': 1.0,
-        'odds_subtree_%': 1.0,
-        'odds_subtree_/': 1.0,
-        'odds_subtree_*': 1.0,
-    }
-
-
-def create_gene_local_settings():
-    return {
-        'depth': 0,
-    }
-
-
-def create_gene(feature_count) -> RootNode:
-    global_settings = create_gene_global_settings(feature_count)
-    local_settings = create_gene_local_settings()
-    threshold = create_gene_threshold()
-    child = create_gene_inner(local_settings, global_settings)
+def create_gene(settings) -> RootNode:
+    threshold = create_gene_threshold(settings.copy())
+    child = create_gene_inner(settings.copy())
     return RootNode(threshold, child)
 
 
@@ -289,38 +198,36 @@ def iterate_gene(gene, parent=None, path=None) -> \
     yield gene, parent, path
 
 
-def crossover(mother: RootNode, father: RootNode, max_depth: int) -> \
+def crossover(mother: RootNode, father: RootNode, settings) -> \
         Tuple[RootNode, RootNode]:
-    def random_node(gene: RootNode) -> Tuple[List[Node], int, int]:
+    def random_node(gene: RootNode) -> Tuple[List[Node], int, int, int]:
         child, parent, path = random.choice(list(iterate_gene(gene)))
         size = max((len(path) for *_, path in iterate_gene(child)), default=0)
-        return parent.children, path[-1], size
+        return parent.children, path[-1], size, len(path)
 
     offspring_1 = copy.deepcopy(mother)
-    root_1, index_1, size_1 = random_node(offspring_1)
-
     offspring_2 = copy.deepcopy(father)
-    root_2, index_2, size_2 = random_node(offspring_2)
 
-    while (size_2 + size_1) > max_depth:
-        root_1, index_1, size_1 = random_node(offspring_1)
-        root_2, index_2, size_2 = random_node(offspring_2)
+    root_1, index_1, size_1, depth_1 = random_node(offspring_1)
+    root_2, index_2, size_2, depth_2 = random_node(offspring_2)
+    max_depth = settings['max_depth']
+
+    while (size_1 + depth_2) > max_depth or (size_2 + depth_1) > max_depth:
+        root_1, index_1, size_1, depth_1 = random_node(offspring_1)
+        root_2, index_2, size_2, depth_2 = random_node(offspring_2)
 
     root_1[index_1], root_2[index_2] = root_2[index_2], root_1[index_1]
 
     return offspring_1, offspring_2
 
 
-def mutate(gene: RootNode, mutation_chance, feature_count):
+def mutate(gene: RootNode, mutation_chance, settings):
     gene = copy.deepcopy(gene)
     for child, parent, path in iterate_gene(gene):
         if random.uniform(0, 1) < mutation_chance:
-            global_settings = create_gene_global_settings(feature_count)
-            local_settings = create_gene_local_settings()
-            local_settings['depth'] = len(path)
-            parent.children[path[-1]] = create_gene_inner(local_settings, global_settings)
+            parent.children[path[-1]] = create_gene_inner(settings.copy(), len(path))
     if random.uniform(0, 1) < mutation_chance:
-        gene.threshold = create_gene_threshold()
+        gene.threshold = create_gene_threshold(settings.copy())
     return gene
 
 
@@ -340,18 +247,29 @@ def main():
     crossover_chance = 0.85
     mutation_chance = 0.0125
     tournament_size = 5
-    max_depth = 4
+    tree_settings = {
+        'max_depth': 8,
+        'threshold_max': 1.0,
+        'threshold_min': -1.0,
+        'constant_max': 1.0,
+        'constant_min': -1.0,
+        'odds_subtree': 2.0,
+        'odds_leaf': 1.0,
+        'odds_leaf_feature': 1.0,
+        'odds_leaf_constant': 1.0,
+        'odds_leaf_features_sum': 1.0,
+        'odds_subtree_+': 1.0,
+        'odds_subtree_%': 1.0,
+        'odds_subtree_/': 1.0,
+        'odds_subtree_*': 1.0,
+        'feature_count': len(features[0]),
+    }
 
-    population = [create_gene(len(features[0])) for _ in range(population_size)]
+    population = [create_gene(tree_settings) for _ in range(population_size)]
     overall_best, overall_best_fitness = None, -float('inf')
 
-    # def predict(f0, f1, f2, f3, f4):
-    #     return (((((((f1 + f2) + (f4 + f0)) % -0.4084233263502599) + f3) % 0.4290489863403546) % 0.5744925820063951) + 0.2948463671644945) > 0.5074617999721471
-    # for f, l in zip(features, labels):
-    #     print(f, predict(*f), l)
-    # return
-
     for generation in range(generation_count):
+        # evaluation
         fitnesses = [gene.fitness(features, labels) for gene in population]
         best_fitness = max(fitnesses)
         best = population[fitnesses.index(best_fitness)]
@@ -366,27 +284,29 @@ def main():
                 return
 
         print(generation, 'Best:', overall_best_fitness, 'Mean:', mean)
+
+        # elitism
         population[fitnesses.index(min(fitnesses))] = copy.deepcopy(overall_best)
 
-        def select():
-            return tournament_selection(population, fitnesses, tournament_size)
-
-        # todo: tuple selection + crossover
-        # crossover
+        # selection
         population = [
-            crossover(select(), select(), max_depth)[0]
-            if random.random() < crossover_chance else select()
+            tournament_selection(population, fitnesses, tournament_size)
             for _ in range(population_size)
         ]
 
+        # crossover
+        middle = population_size // 2
+        population = list(itertools.chain(*(
+            crossover(mother, father, tree_settings)
+            if random.random() < crossover_chance else (mother, father)
+            for mother, father in zip(population[:middle], population[middle:])
+        )))
+
         # mutation
         population = [
-            mutate(gene, mutation_chance, len(features[0]))
+            mutate(gene, mutation_chance, tree_settings)
             for gene in population
         ]
-
-        # population.extend((create_gene(len(features[0])) for _ in range(50)))
-        # population = [compress_gene(gene) for gene in population]
 
 
 if __name__ == '__main__':
