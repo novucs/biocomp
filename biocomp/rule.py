d51fecd22b4669cc6e383b92ec3c29de62585423
novucs <contact@novucs.net>
2019-10-29 16:41:21 +0000

    Solves data2 with 52 rules

--- biocomp/rule.py
+++ biocomp/rule.py
@@ -6,21 +6,22 @@ from biocomp import datasets
 
 class GA:
     def __init__(self):
-        self.dataset = 'datasets/2019/data1.txt'
+        self.dataset = 'datasets/2019/data2.txt'
         train_x, train_y, *_ = datasets.split(
             datasets.load_dataset(self.dataset, datasets.parse_binary_string_features))
         self.rule_count = len(train_x)
         self.rule_size = len(train_x[0]) + 1
-        self.population_size = 50
+        self.population_size = 100
         self.generation_count = 10000
         self.crossover_chance = 0.5
-        # self.mutation_chance = 0.0125
         self.tournament_size = 5
+        self.distill_inheritance_chance = 0.33
         self.train_x = train_x
         self.train_y = train_y
         self.population = []
         self.overall_best = None
         self.overall_best_fitness = -float('inf')
+        self.generation = 0
 
     @property
     def chromosome_size(self):
@@ -29,13 +30,14 @@ class GA:
     @property
     def mutation_chance(self):
         return 1 / self.chromosome_size
+        # return 0.01
 
     def random_chromosome(self, index):
         return random.choice(
             [0, 1, '#'] if index % self.rule_size != (self.rule_size - 1) else [0, 1])
 
     def load_population(self):
-        default_best, default_best_rule_count = None, self.rule_count
+        best, best_rule_count, best_fitness = None, self.rule_count, -float('inf')
         with open('solutions.txt', 'r') as f:
             for line in f:
                 tags = {
@@ -43,37 +45,53 @@ class GA:
                     for tag in line.strip().split(' ')
                 }
 
-                if tags['dataset'] != self.dataset or default_best_rule_count < int(
-                        tags['rule_count']):
-                    continue
+                same_dataset = tags['dataset'] == self.dataset
+                better_rule_count = int(tags['rule_count']) < best_rule_count
+                equal_rule_count = int(tags['rule_count']) == best_rule_count
+                better_fitness = equal_rule_count and float(tags['fitness']) < best_fitness
 
-                default_best = list(
-                    map(lambda k: '#' if k == '#' else int(float(k)),
-                        tags['rules'].split(',')))
-                default_best_rule_count = len(default_best) // self.rule_size
+                if same_dataset and (better_rule_count or better_fitness):
+                    best = list(map(lambda k: '#' if k == '#' else int(float(k)),
+                                    tags['rules'].split(',')))
+                    best_rule_count = len(best) // self.rule_size
+                    best_fitness = float(tags['fitness'])
 
-        if not default_best:
-            default_best = list(itertools.chain(*[
+        self.rule_count = best_rule_count - 1 if best_fitness == 1.0 else best_rule_count
+        self.generate_population(best, smaller=best is not None and best_fitness == 1.0)
+
+    def generate_population(self, original_best=None, smaller=True):
+        if not original_best:
+            original_best = list(itertools.chain(*[
                 list(map(int, tx)) + [int(ty)]
                 for tx, ty in zip(self.train_x, self.train_y)
             ]))
 
-        self.rule_count = default_best_rule_count - 1
-        self.population = [
-            [self.random_chromosome(i) for i in range(self.chromosome_size)]
-            for _ in range(self.population_size - 1)
-        ]
-        self.population.append(default_best[:self.rule_size * self.rule_count])
+        self.population = [original_best[:self.chromosome_size]]
 
-    def evaluate(self, chromosome, features):
-        final_prediction = [0, 0]
+        if smaller:
+            self.population.append(original_best[self.rule_size:])
+
+        for _ in range(self.population_size - (2 if smaller else 1)):
+            rules = []
+
+            for index in range(0, self.chromosome_size, self.rule_size):
+                if random.random() < self.distill_inheritance_chance:
+                    rules.append(original_best[index: index + self.rule_size])
+                else:
+                    rules.append([self.random_chromosome(i) for i in range(self.rule_size)])
+
+            self.population.append(list(itertools.chain(*rules)))
+
+    def evaluate(self, chromosome, attributes):
+        # votes = [0, 0]
         for index in range(0, self.chromosome_size, self.rule_size):
-            *rule, prediction = chromosome[index: index + self.rule_size]
-            if all(p == f or p == "#" for p, f in zip(rule, features)):
-                final_prediction[prediction] += 1
-                # return prediction
-        return final_prediction.index(max(final_prediction))
-        # return 0
+            # print(chromosome)
+            *condition, action = chromosome[index: index + self.rule_size]
+            if all(p == f or p == "#" for p, f in zip(condition, attributes)):
+                # votes[action] += 1
+                return action
+        # return votes.index(max(votes))
+        return None
 
     def fitness(self, chromosome, features, labels):
         return sum(1 if self.evaluate(chromosome, f) == l else 0
@@ -83,9 +101,10 @@ class GA:
         self.load_population()
 
         for generation in range(self.generation_count):
-            self.train_step(generation)
+            self.generation = generation
+            self.train_step()
 
-    def train_step(self, generation):
+    def train_step(self):
         # self.population.append([
         #     0, 0, '#', '#', '#', 1, 1,
         #     1, 1, 1, '#', '#', '#', 1,
@@ -102,16 +121,18 @@ class GA:
         mean_fitness = total_fitness / self.population_size
 
         if best_fitness > self.overall_best_fitness:
-            regenerated_population = self.found_new_best(best, best_fitness, generation)
+            regenerated_population = self.found_new_best(best, best_fitness)
             if regenerated_population:
                 return
+            self.save_solution(best, best_fitness)
 
-        print(
-            f'Generation: {generation:4} \t'
-            f'Best Fitness: {best_fitness:.3f} \t'
-            f'Mean Fitness: {mean_fitness:.3f} \t'
-            f'Rule Count: {self.rule_count:3} \t'
-        )
+        if self.generation % 5 == 0:
+            print(
+                f'Generation: {self.generation:4} \t'
+                f'Best Fitness: {best_fitness:.3f} \t'
+                f'Mean Fitness: {mean_fitness:.3f} \t'
+                f'Rule Count: {self.rule_count:3} \t'
+            )
 
         def select_parent():
             fittest, fitness = None, -1
@@ -138,35 +159,36 @@ class GA:
 
         self.population.append(self.overall_best)
 
-    def found_new_best(self, best, best_fitness, generation):
+    def found_new_best(self, best, best_fitness):
         self.overall_best = best
         self.overall_best_fitness = best_fitness
 
         if best_fitness != 1.0:
             return False
 
-        lines = [f'Found rule in {generation} generations '
+        lines = [f'Found rule in {self.generation} generations '
                  f'with rule count of {self.rule_count}:']
 
+        self.save_solution(best, best_fitness)
+
+        print('\n'.join(lines))
+
+        self.overall_best = None
+        self.overall_best_fitness = -float('inf')
+        self.rule_count -= 1
+        self.generate_population(best)
+        return True
+
+    def save_solution(self, best, fitness):
         with open('solutions.txt', 'a') as f:
             f.write(
                 f'dataset:{self.dataset} '
                 f'rule_count:{self.rule_count} '
-                f'generation:{generation} '
-                f'rules:{",".join(map(str, best))}'
+                f'generation:{self.generation} '
+                f'fitness:{fitness} '
+                f'rules:{",".join(map(str, best))} '
                 f'\n'
             )
-
-        print('\n'.join(lines))
-        self.overall_best = None
-        self.overall_best_fitness = -float('inf')
-        self.rule_count -= 1
-        self.population = [
-            [self.random_chromosome(i) for i in range(self.chromosome_size)]
-            for _ in range(self.population_size - 1)
-        ]
-        self.population.append(best[:self.chromosome_size])
-        return True
 
 
 def main():
