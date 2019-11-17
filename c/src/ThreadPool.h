9b82465f2d4d44d4ad1354c6d9a6db7fae371349
novucs <contact@novucs.net>
2019-11-17 05:49:41 +0000

    Remove unused threadpool map

--- c/src/ThreadPool.h
+++ c/src/ThreadPool.h
@@ -22,26 +22,6 @@ public:
     virtual ~ThreadPool();
 
     void submit(std::function<void()> job);
-
-    template<typename T>
-    std::vector<T> map(std::vector<std::function<T()>> submitted) {
-        std::vector<T> results;
-        results.reserve(submitted.size());
-        std::mutex map_mutex;
-
-        for (int i = 0; i < submitted.size(); i++) {
-            std::function<T()> function = submitted.at(i);
-            submit([&function, &map_mutex, &results, &i] {
-                T result = function();
-                {
-                    std::unique_lock<std::mutex> lock(map_mutex);
-                    results.at(i) = result;
-                }
-            });
-        }
-
-        return results;
-    }
 };
 
 #endif //C_THREADPOOL_H
