b7f3e4b6fb56deb96a56c6d8f3531a9603b29000
novucs <contact@novucs.net>
2019-11-19 22:32:05 +0000

    Data4 solution

--- biocomp/visualise_solution.py
+++ biocomp/visualise_solution.py
@@ -1,7 +1,39 @@
+from biocomp import datasets
+
 # solution = "#,0,0,0,1,#,1,0,#,#,#,1,0,0,1,1,1,1,#,#,1,0,0,1,1,#,#,0,1,0,1,0,0,#,0,1,0,0,1,0,#,1,#,#,#,#,0,1,0,0,1,1,0,#,#,0,#,#,1,1,#,1,1,0,1,0,1,#,#,0,0,0,0,0,#,#,0,#,#,0,0,1,0,1,1,0,#,#,#,1,0,1,1,0,0,#,#,0,#,#,1,1,1,#,0,#,1,#,#,1,0,0,#,#,#,#,#,#,1"
-solution = "1,1,0,1,1,0,0,0,1,1,1,0,1,0,0,0,#,1,1,0,0,0,1,1,0,1,0,1,0,0,1,0,0,0,1,0,1,0,1,0,0,0,#,1,1,1,#,1,1,1,1,1,0,0,0,1,#,0,1,0,0,#,0,0,#,1,#,0,1,1,1,1,0,0,1,1,0,1,0,0,1,0,0,0,#,#,1,#,0,1,0,0,0,#,0,#,1,1,1,1,#,0,#,1,1,#,1,1,0,#,#,0,#,#,0,1,#,0,1,1,1,#,#,1,#,1,0,0,#,#,0,#,0,0,#,1,#,1,#,0,#,#,0,0,1,1,1,#,#,#,1,1,0,0,#,#,#,0,#,1,0,0,#,#,#,0,#,1,#,1,#,#,#,#,0,#,#,#,#,0,#,1,1,#,0,#,#,#,0,#,#,#,#,#,#,1"
-rule_size = 7
-solution = [int(float(s)) if s != '#' else '#' for s in solution.split(',')]
+# solution = "1,1,0,1,1,0,0,0,1,1,1,0,1,0,0,0,#,1,1,0,0,0,1,1,0,1,0,1,0,0,1,0,0,0,1,0,1,0,1,0,0,0,#,1,1,1,#,1,1,1,1,1,0,0,0,1,#,0,1,0,0,#,0,0,#,1,#,0,1,1,1,1,0,0,1,1,0,1,0,0,1,0,0,0,#,#,1,#,0,1,0,0,0,#,0,#,1,1,1,1,#,0,#,1,1,#,1,1,0,#,#,0,#,#,0,1,#,0,1,1,1,#,#,1,#,1,0,0,#,#,0,#,0,0,#,1,#,1,#,0,#,#,0,0,1,1,1,#,#,#,1,1,0,0,#,#,#,0,#,1,0,0,#,#,#,0,#,1,#,1,#,#,#,#,0,#,#,#,#,0,#,1,1,#,0,#,#,#,0,#,#,#,#,#,#,1"
+# rule_size = 7
+# solution = [int(float(s)) if s != '#' else '#' for s in solution.split(',')]
+
+# solution learned from digital data
+"""
+##01#####0 1
+#####0###0 0
+##1####0#1 0
+##1####### 1
+######1##1 1
+########## 0
+"""
+
+# solution learned from floating data
+"""
+##01#####0 1
+##1##1###0 1
+##1####0#1 0
+##1######1 1
+######1##1 1
+########## 0
+"""
+
+rule_size = 11
+solution = """
+##1####1#1 1
+##0###1##1 1
+##1##1###0 1
+##01#####0 1
+########## 0
+""".strip().replace(" ", "").replace("\n", "")
+solution = [int(float(s)) if s != '#' else '#' for s in solution]
 
 
 def print_solution():
@@ -35,7 +67,6 @@ def check_against_data2():
         prediction = evaluate(solution, attributes)
         print('\tAttributes:', ''.join(map(str, attributes)), 'Predicted Label:', prediction)
 
-    from biocomp import datasets
     train_x, train_y, *_ = datasets.split(datasets.load_dataset_2())
     correct = 0
     for features, label in zip(train_x, train_y):
@@ -61,12 +92,38 @@ def print_hard_coded_rules():
         print(f'\t{rule_string} {rule[-1]} {int(rule_string, 2)}')
 
 
+def check_against_digital_data4():
+    X, y, *_ = datasets.split(datasets.load_dataset(
+        "../datasets/2019/digital_data4.txt", datasets.parse_binary_string_features))
+
+    failed_predictions = []
+
+    for features, label in zip(X, y):
+        for i, j in enumerate(range(0, len(solution), rule_size)):
+            *condition, prediction = solution[j:j + rule_size]
+
+            if ''.join(map(str, map(int, features))) == '0000001011' and ''.join(map(str, condition)) == '######1##1':
+                print("")
+
+            if all(c == f or c == '#' for c, f in zip(condition, features)):
+                if prediction != label:
+                    failed_predictions.append((features, condition, label))
+                break
+
+    print(f"Classified {len(X) - len(failed_predictions)} / {len(X)}")
+
+    for features, condition, label in failed_predictions:
+        print(''.join(map(str, condition)), "did not predict",
+              ''.join(map(str, map(int, features))), int(label))
+
+
 def main():
     print_solution()
-    print()
-    check_against_data2()
-    print()
-    print_hard_coded_rules()
+    # print()
+    # check_against_data2()
+    # print()
+    # print_hard_coded_rules()
+    check_against_digital_data4()
 
 
 if __name__ == '__main__':
