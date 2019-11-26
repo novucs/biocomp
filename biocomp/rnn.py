97910c50010e531312d5ac4ef65573702c336640
novucs <contact@novucs.net>
2019-11-26 11:39:32 +0000

    Cleanup rnn

--- biocomp/rnn.py
+++ biocomp/rnn.py
@@ -30,7 +30,6 @@ def net():
         a1 = rnn(rnn_size, features, weights[:rnn_weight_count])
         a2 = activate(dot(a1, weights[rnn_weight_count:])[0])
         prediction = 1 if a2 > 0.5 else 0
-        # print(a2)
         return prediction
 
     total_weight_count = rnn_weight_count + rnn_size
@@ -49,23 +48,11 @@ def main():
     population = [[rand_weight() for _ in range(gene_size)]
                   for _ in range(population_size)]
 
-    # population[0] = [0.5, 0.5, 1.0, 0.0, 1.0, -1.0, 0.0, -0.5, 0.0, 0.5, 1.0, 0.0, 0.5, -0.5, 0.5, -0.5, 0.0, -0.5, 1.0, 0.0, 1.0, 0.5, -0.5, 0.0]
-    # population[1] = [0.5624719564907368, 0.01579871722570525, 0.8948303472886368, 0.6157340941722174, 0.9949089949082408, -0.9718687426685406, -0.04012044940035153, 1.929627787275046, 0.0, 1.5941283662940395, 0.9698990198034685, -0.5937242537595626, 0.5076766524913334, 0.31496156255221663, 0.57901642370433, -1.8955744904976095, -1.29001620980149, 1.8529476938214517, 1.534186975032509, 1.6210843111881692, 0.7812971429782949, 1.8966908800829234, 1.0429150609525122, 0.18841093752313087]
-
     generation_count = 10000
     tournament_size = 5
     crossover_chance = 0.25
     mutation_chance = 0.01
     best = []
-
-    # # Attempt at brute force searching...
-    # b, y = None, -float('inf')
-    # import itertools
-    # for p in itertools.product([-1.0, -0.5, 0.0, 0.5, 1.0], repeat=24):
-    #     fns = sum(int(predict(f, p) == l) for f, l in zip(features, labels))
-    #     if fns > y:
-    #         b, y = p, fns
-    #         print(y)
 
     for generation in range(generation_count):
         fitnesses = [sum(int(predict(f, g) == l) for f, l in zip(features, labels))
